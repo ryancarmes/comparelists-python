eab61b7e826ac58f25d1cc38b98e5012e27e808a
Ryan Armes <ryancarmes@gmail.com>
2018-03-08 14:33:16 -0500

    first commit

---
+++ comparelist.py
@@ -0,0 +1,8 @@
+list_one = [1, 2, "skip a few", 99, 100]
+list_two = [1, 2, 3, 4, 5, 6]
+
+
+if list_one == list_two:
+    print "The lists are the same"
+else:
+    print "The lists are not the same"
